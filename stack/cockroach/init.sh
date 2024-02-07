7c3f8f0ff988afa1e297c8442b9806ef50968eed
Wojciech Holisz <wojciech.holisz@sperasoft.com>
2024-02-07 11:56:32 +0100

    Migrate to Kubernetes

--- stack/cockroach/init.sh
+++ stack/cockroach/init.sh
@@ -2,5 +2,20 @@
 
 cd "$(dirname "$0")"
 
-docker exec -it demo-stack-cockroach-1-1 ./cockroach init --host=cockroach-1:26357 --insecure
-docker exec -i demo-stack-cockroach-1-1 ./cockroach sql --host=cockroach-1:26257  --insecure < init.sql
+mkdir -p certs
+rm -fr certs/*.crt certs/*.key
+
+docker run -it -v $(pwd)/certs:/certs registry.com.gecko/cockroach cert create-ca \
+    --certs-dir=/certs \
+    --ca-key=/certs/ca.key
+
+docker run -it -v $(pwd)/certs:/certs registry.com.gecko/cockroach cert create-node localhost $(hostname) \
+    --certs-dir=/certs \
+    --ca-key=/certs/ca.key
+
+docker run -it -v $(pwd)/certs:/certs registry.com.gecko/cockroach cert create-client root \
+    --certs-dir=/certs \
+    --ca-key=/certs/ca.key
+
+# docker exec -it demo-stack-cockroach-1-1 ./cockroach init --certs-dir=/certs --host=cockroach-1:26357
+# docker exec -i demo-stack-cockroach-1-1 ./cockroach sql --certs-dir=/certs --host=cockroach-1:26257 < init.sql
