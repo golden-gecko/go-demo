b5f0a84f39d5a9ced4808c73e445f50b16415891
Wojciech Holisz <golden.gecko@holisz.pl>
2024-02-12 13:41:51 +0100

    Migrate to Kubernetes

--- stack/cockroach/init_cluster.sh
+++ stack/cockroach/init_cluster.sh
@@ -2,6 +2,5 @@
 
 cd "$(dirname "$0")"
 
-docker exec -it demo-stack-cockroach-1-1 ./cockroach init --certs-dir=/certs --host=cockroach-1:26357
-
-docker exec -i demo-stack-cockroach-1-1 ./cockroach sql --certs-dir=/certs --host=cockroach-1:26257 < init.sql
+docker exec -it demo-stack-cockroach-1-1 ./cockroach init --certs-dir=/certs/node-1 --host=cockroach-1:26357
+docker exec -i demo-stack-cockroach-1-1 ./cockroach sql --certs-dir=/certs/node-1 --host=cockroach-1:26257 < init.sql
--- stack/nakama/entrypoint.sh
+++ stack/nakama/entrypoint.sh
@@ -2,5 +2,11 @@
 
 cd "$(dirname "$0")"
 
-/nakama/nakama migrate up --database.address root@cockroach-1:26257
-/nakama/nakama --name nakama1 --database.address root@cockroach-1:26257 --logger.level DEBUG --session.token_expiry_sec 7200 --metrics.prometheus_port 9104
+/nakama/nakama migrate up \
+    --database.address 'root@cockroach-1:26257?sslmode=require&sslrootcert=/certs/ca.crt&sslcert=/certs/client.root.crt&sslkey=/certs/client.root.key' 
+
+/nakama/nakama --name nakama1 \
+    --database.address 'root@cockroach-1:26257?sslmode=require&sslrootcert=/certs/ca.crt&sslcert=/certs/client.root.crt&sslkey=/certs/client.root.key' \
+    --logger.level DEBUG \
+    --session.token_expiry_sec 7200 \
+    --metrics.prometheus_port 9104
