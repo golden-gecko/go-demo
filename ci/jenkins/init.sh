a8259b551682fcb16f4b8dae2e4ec4a37c726e1d
Wojciech Holisz <golden.gecko@holisz.pl>
2024-02-19 12:58:54 +0100

    Add Nomad

--- ci/jenkins/init.sh
+++ ci/jenkins/init.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"
 
-docker exec -it ci-jenkins-1 sh -c "ssh-keygen -b 2048 -t rsa -f /var/jenkins_home/.ssh/id_rsa -q -N ''"
-docker exec -it ci-jenkins-1 sh -c "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+docker exec -it demo-ci-jenkins-1 sh -c "ssh-keygen -b 2048 -t rsa -f /var/jenkins_home/.ssh/id_rsa -q -N ''"
+docker exec -it demo-ci-jenkins-1 sh -c "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
