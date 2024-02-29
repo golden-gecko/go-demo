b5909206f2246a1f53abe9a6309fc2ed8331a4f7
Wojciech Holisz <wojciech.holisz@sperasoft.com>
2024-02-29 13:45:47 +0100

    Add Kubernetes

--- deployments/kubernetes/init_deployments.sh
+++ deployments/kubernetes/init_deployments.sh
@@ -2,6 +2,9 @@
 
 cd "$(dirname "$0")"
 
+kubectl apply --filename volumes/roach-claim.yaml
+kubectl apply --filename volumes/roach.yaml
+
 kubectl apply --filename deployments/api.yaml
 kubectl apply --filename deployments/node.yaml
 kubectl apply --filename deployments/roach.yaml
