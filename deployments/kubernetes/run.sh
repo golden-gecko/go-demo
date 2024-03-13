cc29eca39b40841ff14918f0eac76e1165a9e4e2
Wojciech Holisz <golden.gecko@holisz.pl>
2024-03-13 13:02:26 +0100

    Add Kubernetes

--- deployments/kubernetes/run.sh
+++ deployments/kubernetes/run.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "$0")"
 
 find deployments -name "*.yaml" -exec kubectl apply --filename {} \;
+find ingress -name "*.yaml" -exec kubectl apply --filename {} \;
 find pods -name "*.yaml" -exec kubectl apply --filename {} \;
 find services -name "*.yaml" -exec kubectl apply --filename {} \;
 find volumes -name "*.yaml" -exec kubectl apply --filename {} \;
