f614cb1248bb164062addf9346514d212a2b08ad
golden-gecko <golden-gecko@holisz.pl>
2025-11-22 12:54:22 +0100

    Add replicas and balancing

--- aggregator/main.go
+++ aggregator/main.go
@@ -14,40 +14,46 @@ import (
 	"aggregator/roach"
 )
 
-func ProcessVehicleCountByModel(client *redis.Client) {
+func ProcessVehicleCountByModel(client *redis.Client) error {
     models, err := roach.GetVehicleCountByModel()
 
     if err != nil {
-        log.Error(err)
-        os.Exit(1)
+		return err
     }
 
     for _, model := range models {
-        err := client.Set(context.Background(), fmt.Sprintf("%s %s", model.Brand, model.Model), strconv.Itoa(model.Count), 0).Err()
+        err := client.Set(context.Background(), fmt.Sprintf("model_%s_%s", model.Brand, model.Model), strconv.Itoa(model.Count), time.Minute).Err()
 
         if err != nil {
-            log.Error(err)
-            os.Exit(1)
+			return err
         }
     }
+
+	return nil
 }
 
-func ProcessVehicleCountByYear(client *redis.Client) {
+func ProcessVehicleCountByYear(client *redis.Client) error {
     years, err := roach.GetVehicleCountByYear()
 
     if err != nil {
-        log.Error(err)
-        os.Exit(1)
+		return err
     }
 
     for _, year := range years {
-        err := client.Set(context.Background(), strconv.Itoa(year.Year), strconv.Itoa(year.Count), 0).Err()
+        err := client.Set(context.Background(), fmt.Sprintf("year_%d", year.Year), strconv.Itoa(year.Count), time.Minute).Err()
 
         if err != nil {
-            log.Error(err)
-            os.Exit(1)
+			return err
         }
     }
+
+	return nil
+}
+
+func Sleep(interval int) {
+	log.Info("Sleeping for ", interval, " ms")
+
+	time.Sleep(time.Duration(interval) * time.Millisecond)
 }
 
 func main() {
@@ -69,13 +75,16 @@ func main() {
 	defer client.Close()
 
     for {
-        ProcessVehicleCountByModel(client)
-        ProcessVehicleCountByYear(client)
+        if err := ProcessVehicleCountByModel(client); err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
 
-		interval := 3000
+        if err := ProcessVehicleCountByYear(client); err != nil {
+			log.Error(err)
+			os.Exit(1)
+		}
 
-        log.Info("Sleeping for ", interval, " ms")
-
-        time.Sleep(time.Duration(interval) * time.Millisecond)
+		Sleep(3000)
     }
 }
