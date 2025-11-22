885546afeaa44df36a759cb1ba02e1cea9d2694f
golden-gecko <golden-gecko@holisz.pl>
2025-11-22 12:54:24 +0100

    Add nginx proxy

--- deploy/ci/nginx/init.sh
+++ deploy/ci/nginx/init.sh
@@ -3,27 +3,28 @@
 cd "$(dirname "$0")"
 cd ssl
 
-openssl genrsa 2048 > ca-key.pem
+openssl genrsa \
+    -out ca-key.pem 2048
 
-openssl req -new -x509 -nodes -days 365000 -subj "/CN=com.gecko" \
+openssl req -new -x509 -nodes -days 365 -subj "/C=PL/L=Cracow/O=GoldenGecko/CN=gecko" \
    -key ca-key.pem \
    -out ca-cert.pem
 
-openssl req -newkey rsa:2048 -nodes -days 365000 -subj "/CN=*.com.gecko" \
+openssl req -newkey rsa:2048 -nodes -days 365 -subj "/C=PL/L=Cracow/O=GoldenGecko/CN=com.gecko" \
    -keyout server-key.pem \
    -out server-req.pem
 
-openssl x509 -req -days 365000 -set_serial 01 \
+openssl x509 -req -days 365 -set_serial 01 \
    -in server-req.pem \
    -out server-cert.pem \
    -CA ca-cert.pem \
    -CAkey ca-key.pem
 
-openssl req -newkey rsa:2048 -nodes -days 365000 -subj "/CN=registry.com.gecko" \
+openssl req -newkey rsa:2048 -nodes -days 365 -subj "/C=PL/L=Cracow/O=GoldenGecko/CN=*.com.gecko" \
    -keyout client-key.pem \
    -out client-req.pem
 
-openssl x509 -req -days 365000 -set_serial 01 \
+openssl x509 -req -days 365 -set_serial 01 \
    -in client-req.pem \
    -out client-cert.pem \
    -CA ca-cert.pem \
@@ -38,4 +39,5 @@ openssl verify -CAfile ca-cert.pem \
    client-cert.pem
 
 # Chain
-cat client-cert.pem server-cert.pem ca-cert.pem > client-chain.pem
+cat server-cert.pem ca-cert.pem > server-chain.pem
+cat client-cert.pem server-cert.pem > client-chain.pem
